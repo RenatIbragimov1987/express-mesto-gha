5b05f45903df9b2ed3c2e0ba8082edbe55f21b94
Renat Ibragimov <naka070787@gmail.com>
2022-05-22 15:22:56 +0300

    commit

--- app.js
+++ app.js
@@ -14,53 +14,49 @@ const NotFoundDataError = require('./errors/NotFoundDataError');
 
 const app = express();
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/mestodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: false,
-  });
-  // app.use(helmet());
-  app.use(cookieParser());
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+// app.use(helmet());
+app.use(cookieParser());
 
-  app.use(express.json());
+app.use(express.json());
 
-  app.post('/signin', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }), login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
-  app.post('/signup', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/(http|https):\/\/(www)?\.?([A-Za-z0-9.-]+)\.([A-z]{2,})((?:\/[+~%/.\w-_]*)?\??(?:[-=&;%@.\w_]*)#?(?:[\w]*))?/),
-    }),
-  }), createUser);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/(http|https):\/\/(www)?\.?([A-Za-z0-9.-]+)\.([A-z]{2,})((?:\/[+~%/.\w-_]*)?\??(?:[-=&;%@.\w_]*)#?(?:[\w]*))?/),
+  }),
+}), createUser);
 
-  app.use(isAuth);
+app.use(isAuth);
 
-  app.use('/', users);
-  app.use('/', cards);
+app.use('/', users);
+app.use('/', cards);
 
-  app.use((req, res, next) => {
-    next(new NotFoundDataError('Запрошен несуществующий маршрут'));
-  });
+app.use((req, res, next) => {
+  next(new NotFoundDataError('Запрошен несуществующий маршрут'));
+});
 
-  app.use(errors());
+app.use(errors());
 
-  app.use((err, req, res, next) => {
-    const { statusCode = 500, message } = err;
-    res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-    next();
-  });
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  next();
+});
 
-  app.listen(PORT, () => {
-    console.log(`Слушаем ${PORT} порт`);
-  });
-}
-
-main();
+app.listen(PORT, () => {
+  console.log(`Слушаем ${PORT} порт`);
+});
